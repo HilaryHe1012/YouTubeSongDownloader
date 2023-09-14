de0d2a29918e56c8c85364b5caa8c1b979bd73bc
HilaryHe1012 <hilaryhe1012@gmail.com>
2023-09-14 12:18:44 -0400

    runtime improvement

--- YoutubeTomp3.py
+++ YoutubeTomp3.py
@@ -1,28 +1,37 @@
 from pytube import YouTube
-def download(url):
+import threading
+
+def download_video(url, output_path):
     try:
         video = YouTube(url)
-        yt = video 
         stream = video.streams.filter(only_audio=True).first()
-        stream.download(output_path="C:/Users/hilar/Music", filename=f"{video.title} - SnowMan.mp3")
-        print("The video is downloaded in MP3")
-    except KeyError:
-        print("Unable to fetch video information. Please check the video URL or your network connection.")
-    return 
+        stream.download(output_path=output_path, filename=f"{video.title} - SnowMan.mp3")
+        print(f"The video {video.title} is downloaded in MP3")
+    except Exception as e:
+        print(f"Error downloading video from {url}: {e}")
+
+def download_videos(urls, output_path):
+    threads = []
+    for url in urls:
+        thread = threading.Thread(target=download_video, args=(url, output_path))
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
 
 def main():
+    st = time.time()
     way = input("Do you want to download a bunch or just 1? (1/Bunch) \n")
+    output_path = "C:/Users/hilar/Music"
+    
     if way == "1":
         url = input("Please copy your url: \n")
-        download(url)
-    if way == "Bunch":
-        n = input("Enter the number of videos you want to download: \n")
-        urls, i = [], 0
-        while i < int(n):
-            url = input("Please copy your url: \n")
-            i += 1
-            download(url)
-        
-    return 
-
-main()
+        download_video(url, output_path)
+    
+    elif way == "Bunch":
+        n = int(input("Enter the number of videos you want to download: \n"))
+        urls = [input("Please copy your url: \n") for _ in range(n)]
+        download_videos(urls, output_path)
+if __name__ == "__main__":
+    main()
